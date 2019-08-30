993a130bca31e96ca956a1fa176fa2edb483fb8c
ecosse3 <theecosse@gmail.com>
2019-08-30 18:09:35 +0200

    Added Http modules to hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,18 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -21,5 +26,9 @@ export class HeroService {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
